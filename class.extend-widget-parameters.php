56194122c276c02eab5410f66c4b510fe77c1586
kermage <genealyson.torcende@gmail.com>
2016-08-27 00:28:37 +0800

    add extra fields

--- class.extend-widget-parameters.php
+++ class.extend-widget-parameters.php
@@ -24,7 +24,27 @@ if ( ! class_exists( 'Extend_Widget_Parameters' ) ) {
         
         private function __construct() {
             
+            add_action( 'in_widget_form', array( $this, 'add_extra_fields' ), 10, 3 );
             
+        }
+        
+        
+        public static function add_extra_fields( $widget, $return, $instance ) {
+            
+            $instance = wp_parse_args( (array) $instance, array( 'widget-id' => '', 'widget-class' => '' ) );
+            ?>
+            
+            <p>
+                <label for="<?php echo $widget->get_field_id( 'widget-id' ); ?>"><?php _e( 'ID', 'ewp' ); ?>:</label>
+                <input class="widefat" id="<?php echo $widget->get_field_id( 'widget-id' ); ?>" name="<?php echo $widget->get_field_name( 'widget-id' ); ?>" type="text" value="<?php echo $instance['widget-id']; ?>" />
+            </p>
+            
+            <p>
+                <label for="<?php echo $widget->get_field_id( 'widget-class' ); ?>"><?php _e( 'Class', 'ewp' ); ?>:</label>
+                <input class="widefat" id="<?php echo $widget->get_field_id( 'widget-class' ); ?>" name="<?php echo $widget->get_field_name( 'widget-class' ); ?>" type="text" value="<?php echo $instance['widget-class']; ?>" />
+            </p>
+            
+            <?php
         }
         
     }
